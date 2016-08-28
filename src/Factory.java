bb5c3e7764c2885d6c7b0992d7b01be2fde8e665
Fredy Espana 15060 <fredy.espc@gmail.com>
2016-08-28 11:55:34 -0600

    Clase factoy lista

---
+++ src/Factory.java
@@ -0,0 +1,27 @@
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Factory <E> {
+	
+	/* El metodo recibe un numero entero para crear la lista y la retorna */
+	public Set<E> setTable(int type){
+		
+		Set<E> lista = null;
+		
+		switch(type){
+			case 1:
+				lista = new TreeSet<E>();
+				break;
+			case 2:
+				lista = new HashSet<E>();
+				break;
+			case 3:
+				lista = new LinkedHashSet<E>();
+				break;
+		}
+		return lista;
+	}
+
+}
